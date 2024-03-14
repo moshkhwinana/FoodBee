aff1c6bff82f5ad12e7977e80e1f10313e8c4e8e
Finn Mclaughlin <finnmacm@gmail.com>
2024-03-14 11:56:31 +0200

    solve conflict

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,14 +4,17 @@ class RecipeController < ApplicationController
   end
 
   def show
-    @recipe = Recipe.find(params[:id]) # Ensure this matches how you're setting up @recipe
+    @recipe = Recipe.find(params[:id])
+    ingredients = params[:ingredients]&.split(',')&.map(&:strip) || []
+    generator = RecipeGenerator.new(ingredients)
+    @generated_recipe_content = generator.generate
   end
 
   def generate
-    titles = RecipeGenerator.new(params[:ingredients]).generate
+    @titles = RecipeGenerator.new(params[:ingredients]).generate
   end
 
   def create
-    @recipes = Recipe.new(recipe_params)
+    @recipe = Recipe.new(recipe_params)
   end
 end
