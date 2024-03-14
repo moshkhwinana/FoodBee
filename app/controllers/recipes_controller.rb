69fb87ac7d8f7fb7bd5c87bfdadcb2bb6880f512
Finn Mclaughlin <finnmacm@gmail.com>
2024-03-14 10:49:48 +0200

    pushing openai functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,9 +4,11 @@ class RecipeController < ApplicationController
   end
 
   def show
+    @recipe = Recipe.find(params[:id]) # Ensure this matches how you're setting up @recipe
   end
 
   def generate
+    titles = RecipeGenerator.new(params[:ingredients]).generate
   end
 
   def create
