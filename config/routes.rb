ecd42da36a6f0178dad346364eccc1f80838dcab
Moshoeu Khwinana <moshkhwinana@gmail.com>
2024-03-14 17:46:22 +0200

    arrow added to form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :products do
-    resources :recipes
-  end
+  resources :products 
+  resources :recipes
   resources :favourites
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
