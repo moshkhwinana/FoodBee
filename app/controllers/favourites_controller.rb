99500f425991d52147beaa79f2f685f5350236ed
Moshoeu Khwinana <moshkhwinana@gmail.com>
2024-03-18 14:29:40 +0200

    favourites done

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,13 +1,22 @@
 class FavouritesController < ApplicationController
   def index
+    # @favourites = Favourite.all
+    @favourites = current_user.favourites
   end
 
   def create
     @recipe = Recipe.find(params[:recipe_id])
-    current_user.favourites << recipe
-    redirect_to recipes_path, notice: "Recipe favourited"
+    @favourite = Favourite.new(user_id: current_user.id, recipe_id: @recipe.id)
+    if @favourite.save
+      redirect_to favourites_path, notice: "Recipe favourited successfully"
+    else
+      redirect_to recipes_path, alert: "Failed to favourite recipe"
+    end
   end
 
   def destroy
+    @favourite = Favourite.find(params[:id])
+    @favourite.destroy
+    redirect_to favourites_path, status: :see_other
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   root to: "pages#home"
   resources :products
   post "/generate", to: "recipes#generate", as: "generate_recipes"
-  resources :recipes
-  resources :favourites
+  resources :recipes do
+    resources :favourites, only: [:create]
+  end
+  resources :favourites, only: [:index, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
