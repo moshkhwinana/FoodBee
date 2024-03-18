46bc7bae1df32c772d5b338661ee0606184d143e
Samantha Gusha <samanthagusha@icloud.com>
2024-03-18 12:54:55 +0200

    steps logic

--- app/services/recipe_generator.rb
+++ app/services/recipe_generator.rb
@@ -19,7 +19,7 @@ class RecipeGenerator
         content: <<-CONTENT
           I'm creating a cooking app and need to generate recipes based on specific ingredients. The ingredients I have are: #{@ingredients.join(" ")}. Generate exactly 3 simple recipes that utilize these ingredients. For each recipe, provide the following details in a structured format:
           - Recipe title
-          - Steps provided in a list format, starting with "1."
+          - Steps (as an array of steps)
           - Difficulty level out of 5
           - Preparation time
           - Cooking time
@@ -46,26 +46,26 @@ class RecipeGenerator
     end
   end
 
-  # def get_and_save_image_on_recipe(recipe)
-  #   url = generate_image_for_recipe(recipe.recipe_name)
-  #   recipe.update(image_url: url)
-  #   puts "the image url is #{url}"
-  #   recipe
-  #   url
-  # end
+  def get_and_save_image_on_recipe(recipe)
+    url = generate_image_for_recipe(recipe.recipe_name)
+    # recipe.update(image_url: url)
+    puts "the image url is #{url}"
+    # recipe
+    url
+  end
 
 
   private
 
-  # def generate_image_for_recipe(recipe_name)
-  #   prompt = "Generate a visually appealing image of #{recipe_name} made with #{@ingredients.join(' ')}."
+  def generate_image_for_recipe(recipe_name)
+    prompt = "Generate a visually appealing image of #{recipe_name} made with #{@ingredients.join(' ')}."
 
-  #   response = client.images.generate(parameters: { prompt: prompt, size: "256x256" })
-  #   image_url = response.dig("data", 0, "url")
+    response = client.images.generate(parameters: { prompt: prompt, size: "256x256" })
+    image_url = response.dig("data", 0, "url")
 
-  #   puts "Generated image for recipe #{recipe_name}: #{image_url}"
-  #   image_url
-  # end
+    puts "Generated image for recipe #{recipe_name}: #{image_url}"
+    image_url
+  end
 
   def get_client
     @client ||= OpenAI::Client.new
@@ -79,21 +79,22 @@ class RecipeGenerator
     parsed_content["recipes"].map do |recipe|
       recipe_attributes = {
         recipe_name: recipe["recipe_title"],
-        instructions: recipe["recipe_steps"].join("\n"),
+        instructions: recipe["recipe_steps"].map { |step| step.gsub(",", " ") }.join("\n"),
         level: recipe["recipe_difficulty"],
         prep_time: recipe["prep_time"],
         cooking_time: recipe["cooking_time"]
       }
 
-      recipe = Recipe.create!(recipe_attributes)
-      recipe.image_url = generate_image_for_recipe(recipe.recipe_name)
-      url = get_and_save_image_on_recipe(recipe)
-      puts "the image url is now #{url}"
-      recipe.update!(image_url: url)
 
-      # create a recipeingredient record for each @product_name
+      recipe = Recipe.create!(recipe_attributes)
+      # recipe.image_url = generate_image_for_recipe(recipe.recipe_name)
+      # url = get_and_save_image_on_recipe(recipe)
+      # puts "the image url is now #{url}"
+      # recipe.update!(image_url: url)
+
+      # create a recipeingredient record for each product
       # @products.each do |product|
-      #   @recipe.products << product
+      #   recipe.products << product
       # end
 
       recipe
