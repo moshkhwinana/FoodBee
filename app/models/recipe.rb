7bccf1fd10d1fbe6e83991ded1eb7bf5f72b37bd
Moshoeu Khwinana <moshkhwinana@gmail.com>
2024-03-20 15:27:24 +0200

    todds design

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,8 +4,4 @@ class Recipe < ApplicationRecord
   has_many :products, through: :recipeingredients
   has_one_attached :photo
   validates :recipe_name, :instructions, :prep_time, :cooking_time, :level, presence: true
-
-  def favourite(user)
-    user.favourites.include?(self)
-  end
 end
